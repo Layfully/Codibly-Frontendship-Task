ed0e619d1e5f6ed2c483ea3533088a825919e9f2
Adrian Gaborek <adrian.gaborek@student.up.krakow.pl>
2022-06-22 11:23:20 +0200

    mvp: add paginated table with products and filtering by id

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CssBaseline/>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
